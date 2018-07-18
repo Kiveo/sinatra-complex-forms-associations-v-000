13164149267f0cb358e13e178dce7d914c036759
Maverick Tesini <mavericktesini@gmail.com>
2018-07-18 05:37:29 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -11,8 +11,7 @@ class PetsController < ApplicationController
   end
 
   post '/pets' do
-    # @pet = Pet.create(name: params[:pet][:name])
-    @pet = Pet.create(params["pet"])
+    @pet = Pet.create(name: params[:pet][:name])
     # if !params["owner"]["name"].empty?
     #   @pet.owner = Owner.create(name: params["owner"]["name"])
     # end
